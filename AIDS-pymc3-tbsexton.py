041d78dad91bbf0a42bfb39481487ec4cc1b0d2f
Igor Batov <igbatov@gmail.com>
2018-03-04 10:12:04 +0300

    AIDS-pymc3-tbsexton.py

---
+++ AIDS-pymc3-tbsexton.py
@@ -0,0 +1,72 @@
+#
+#   Example for graph e2 --> h1 --> e1
+#    $graph = {
+#      nodes:{'e1':['1','2'], 'e2':['1','2'], 'h1':['1','2']}, // every node contains array of its alternatives
+#      edges:[['h1','e1'],['e2','h1']]
+#    };
+#    $probabilities = {
+#     e1: {
+#       soft:{1:1, 2:0}, // soft evidence for e1 and ^e1
+#       '{"h1":"1"}':{1:0.999, 2:0.001}, // sum must be equal to 1
+#       '{"h1":"2"}':{1:0.001, 2:0.999}  // sum must be equal to 1
+#     },
+#     e2: {
+#       soft:{1:1, 2:0} // soft evidence for e2 and ^e2
+#     },
+#     h1: {
+#       // prior probability of proposition alternative is always 1/<number of alternatives>
+#       '{"e2":"1"}':{1:0.0001, 2:0.9999}, // sum must be equal to 1
+#       '{"e2":"2"}':{1:0.5, 2:0.5}  // sum must be equal to 1
+#      }
+#    }
+#
+# Approach:
+#   https: // github.com / pymc - devs / pymc3 / issues / 1790
+#   https: // gist.github.com / tbsexton / 1349864212b25cce91dbe5e336d794b4
+#
+import numpy as np
+import pandas as pd
+
+import pymc3 as pm
+import theano.tensor as tt
+from theano.compile.ops import as_op
+import theano
+
+with pm.Model() as model:
+    # we will modelling fact's soft evidence as extra parent node of this fact
+    e1_soft_prob = np.array([1, 0])
+    # e1 has 2 parents (soft evidence + h1) and 2 values => we have to define 2x2x2 matrix
+    e1_prob = np.array([
+       [
+           [0.999, 0.001],
+           [0.001, 0.999],
+       ],
+       [
+           [0.5, 0.5],
+           [0.5, 0.5],
+       ],
+    ])
+
+    h1_prob = np.array([
+       [0.0001, 0.9999],
+       [0.5, 0.5],
+    ])
+
+    e2_prob = np.array([1, 0])
+
+    e1_soft = pm.Categorical('e1_soft', p=e1_soft_prob)
+    e2 = pm.Categorical('e2', p=e2_prob, observed=0)
+
+    h1_prob_shared = theano.shared(h1_prob)  # make it global
+    h1_prob_final = h1_prob_shared[e2]
+    h1 = pm.Categorical('h1', p=h1_prob_final)
+
+    e1_prob_shared = theano.shared(e1_prob)  # make it global
+    e1_prob_final = e1_prob_shared[0, h1]  # select the prob array that "happened" thanks to parents
+    e1 = pm.Categorical('e1', p=e1_prob_final, observed=0)
+
+with model:
+    SAMPLE_NUM = 20000
+    trace = pm.sample(SAMPLE_NUM)
+    print(sum(trace['h1']) / float(SAMPLE_NUM))
+   # print(sum(trace['e1']) / float(SAMPLE_NUM))
--- AIDS-pymc3.py
+++ AIDS-pymc3.py
@@ -7,55 +7,22 @@ from pymc3 import Discrete
 from pymc3.distributions.dist_math import bound, binomln, logpow
 
 
-# class I_am_HIV_positive(Discrete):
-#     def __init__(self, f_1_of_10000, *args, **kwargs):
-#         super(I_am_HIV_positive, self).__init__(*args, **kwargs)
-#         self.f_1_of_10000 = tt.as_tensor_variable(f_1_of_10000)
-#
-#     def logp(self, value):
-#         f_1_of_10000 = self.f_1_of_10000
-#         p = ifelse(
-#             tt.eq(f_1_of_10000, 1),
-#             ifelse(tt.eq(value, 1), 0.0001, 0.9999),
-#             tt.cast(0.5000, 'float64')
-#         )
-#
-#         return bound(tt.log(p),
-#             value >= 0, value <= 1,
-#             p >= 0, p <= 1)
-
 class I_am_HIV_positive(Discrete):
-    def __init__(self, f_1_of_10000, ptable, *args, **kwargs):
+    def __init__(self, f_1_of_10000, *args, **kwargs):
         super(I_am_HIV_positive, self).__init__(*args, **kwargs)
         self.f_1_of_10000 = tt.as_tensor_variable(f_1_of_10000)
-        self.ptable = tt.as_tensor_variable(ptable)
 
     def logp(self, value):
-
-        def switch_case(ptable_row, f_1_of_10000, v):
-            tt.printing.Print('ptable_row')(ptable_row)
-            tt.printing.Print('f_1_of_10000')(f_1_of_10000)
-            #print(ptable_row.get_value())
-
-            # f_1_of_10000 = parents[0]
-            # value = parents[1]
-            if (tt.eq(f_1_of_10000, ptable_row[0]) and tt.eq(v, ptable_row[1])):
-                tt.printing.Print('ptable_row[2]')(ptable_row[2])
-                return ptable_row[2]
-            else:
-                return tt.constant(0)
-
-        ptable = self.ptable
-        v = value
         f_1_of_10000 = self.f_1_of_10000
-        components, updates = theano.scan(fn=switch_case, sequences=ptable, non_sequences=[f_1_of_10000, v])
-        tt.printing.Print('components')(components)
-        p = tt.sum(components)
-        tt.printing.Print('p')(p)
+        p = ifelse(
+            tt.eq(f_1_of_10000, 1),
+            ifelse(tt.eq(value, 1), 0.0001, 0.9999),
+            tt.cast(0.5000, 'float64')
+        )
+        p = tt.printing.Print('p')(p)
         return bound(tt.log(p),
             value >= 0, value <= 1,
             p >= 0, p <= 1)
-
 
 class HIV_test_false_positive(Discrete):
     def __init__(self, test_show_positive, p_I_am_HIV_positive, *args, **kwargs):
@@ -66,42 +33,48 @@ class HIV_test_false_positive(Discrete):
     def logp(self, value):
         test_show_positive = self.test_show_positive
         p_I_am_HIV_positive = self.p_I_am_HIV_positive
-        if (tt.eq(test_show_positive, 1)):
-            if (tt.eq(p_I_am_HIV_positive, 1) and tt.eq(value, 1)):
-                p = 0.999
-            elif (tt.eq(p_I_am_HIV_positive, 1) and tt.eq(value, 0)):
-                p = 0.001
-            elif (tt.eq(p_I_am_HIV_positive, 0) and tt.eq(value, 1)):
-                p = 0.001
-            elif (tt.eq(p_I_am_HIV_positive, 0) and tt.eq(value, 0)):
-                p = 0.999
-        elif (tt.eq(test_show_positive, 0)):
-            p = 0.5
-        else:
-            return tt.constant(-np.inf)
-        return tt.constant(np.log(p))
+
+        p = ifelse(
+            tt.eq(test_show_positive, 1),
+            ifelse(
+                tt.eq(p_I_am_HIV_positive, 0),
+                ifelse(
+                    tt.eq(value, 0),
+                    0.999,
+                    0.111
+                ),
+                ifelse(
+                    tt.eq(value, 0),
+                    0.001,
+                    0.999
+                )
+            ),
+            tt.cast(0.5000, 'float64')
+        )
+
+        return bound(tt.log(p),
+            value >= 0, value <= 1,
+            p >= 0, p <= 1)
 
 
 with pm.Model() as model:
-    f_1_of_10000 = pm.Categorical("f_1_of_10000", [0.1, 0.9])
+    f_1_of_10000 = pm.Categorical("f_1_of_10000", np.array([0, 1]))
+    #f_1_of_10000 = tt.printing.Print('f_1_of_10000')(f_1_of_10000)
+    p_I_am_HIV_positive = I_am_HIV_positive('p_I_am_HIV_positive', f_1_of_10000, testval=1)
 
-    #p_I_am_HIV_positive = pm.Binomial('p_I_am_HIV_positive', 1, 0.3)
-    ptable = [
-        [0, 0, 0.5],
-        [0, 1, 0.5],
-        [1, 0, 0.0001],
-        [1, 1, 0.9999],
-    ]
-    p_I_am_HIV_positive = I_am_HIV_positive('p_I_am_HIV_positive', f_1_of_10000, ptable, testval=0)
-
-    # test_show_positive = pm.Categorical("test_show_positive", [0.1, 0.9])
+    # test_show_positive = pm.Categorical("test_show_positive", [0, 1])
     #
-    # p_HIV_test_false_positive = HIV_test_false_positive('p_HIV_test_false_positive', test_show_positive=test_show_positive,
-    #                                                     p_I_am_HIV_positive=p_I_am_HIV_positive, testval=1, observed=1)
+    # p_HIV_test_false_positive = HIV_test_false_positive(
+    #     'p_HIV_test_false_positive',
+    #     test_show_positive,
+    #     p_I_am_HIV_positive,
+    #     testval=1
+    # )
 
-    step = pm.Metropolis()
-    SAMPLE_NUM = 20000
-    trace = pm.sample(SAMPLE_NUM, tune=1000, step=step)
-    s1 = trace['f_1_of_10000']
+ #   step = pm.Metropolis()
+    SAMPLE_NUM = 1000
+#    trace = pm.sample(SAMPLE_NUM, tune=1000, step=step)
+    trace = pm.sample(SAMPLE_NUM)
     samples = trace['p_I_am_HIV_positive']
-    print(sum(samples) / SAMPLE_NUM)
+    print(sum(samples))
+    print(sum(samples) / float(SAMPLE_NUM))
